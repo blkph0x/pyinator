1e3e04b5d79c5054b6ebaa8be467360d6e36b6fc
root <blkph0x@gmail.com>
2018-07-04 23:21:38 +1000

    Fixxed loop and Cleaned Up

--- binance.py
+++ binance.py
@@ -6,110 +6,52 @@ f = open(filename,'a+')
 s = []
 ListOne=[]
 ListTwo=[]
-ii = 3000
 
-def PriceCheck(ListOne, ListTwo):
-	global ii
-	ii += 1000
-	#print(ListOne)
-	sys.setrecursionlimit(ii)
-	for x,y in zip(ListOne,ListTwo):
-		if x != y:
-			print("Price has changed from ",x," ",y)
-			f.write("".join((str(y),'\n')))
-			
-		#print(x)
-		#for y in ListTwo:
-			#print(y)
-			#if y != x:
-				
-			#print('price has changed from ',x,' to ',y)
-				#print(x,"has chanaged to",y)
-		
-		
 
-				
 def GetPrice():
-	
+
 	ListOne=[]
 	ListTwo=[]
 	filename = 'ETHSKY.csv'
-        #f = open(filename,'a+')
-        #r = requests.get('https://www.binance.com/api/v1/ticker/price?symbol=SKYETH')
-        #r = requests.get('https://www.binance.com/api/v1/ticker/price')
-        #DateS = str(datetime.datetime.now())
-        #array = json.loads(json.dumps(r.json()))
-
-	#print("Machine Learning the Crypto World\r\n\r\n")
+	#infinite loop
 	while True:
 		try:
-			
+			#request One
 			r = requests.get('https://www.binance.com/api/v1/ticker/price')
 			time.sleep(0.1)
 			DateS = str(datetime.datetime.now())
 			array = json.loads(r.text)
 			lastprice = array
-			
-			
 			ticker_list=[]
 			
-			#print(array)
-                        #time.sleep(5)
-			s = ""
-
+			#step thugh list appllying symbol and price to ListOne
 			for x in array:
-
-				#ListOne.append(x['price'])
 				ticker_list.extend([x['symbol'] + "," + x['price']])
-			
-			
-			
 			ListOne = ticker_list
 			
 			
-			
+			#Request Two
 			r = requests.get('https://www.binance.com/api/v1/ticker/price')
 			time.sleep(0.1)
 			DateS = str(datetime.datetime.now())
 			array = json.loads(r.text)
 			lastprice = array
-			
-
 			ticker_list=[]
-
-                        #print(array)
-                        #time.sleep(5)
-			s = ""
-
+			#step thugh list appllying symbol and price to ListTwo
 			for x in array:
-
-                                #ListOne.append(x['price'])
 				ticker_list.extend([x['symbol'] + "," + x['price']])
-
-
-
 			ListTwo = ticker_list
+			#step thugh each entry of each list and compair if not eq then price changed
 			for x,y in zip(ListOne,ListTwo):
                 		if x != y:
                         		print("Price has changed from ",x," ",y)
                         		f.write("".join((str(y),'\n')))
-                                                                                                       
-			#PriceCheck(ListOne,ListTwo)
 			ListOne = []
 			ListTwo = []
-						#print(f"",ticker_list[2],end="\r",flush=True)
-                        #for x in ticker_list:
-                                #print(x)
-
-                                #if lastprice != array:
-                                #       f.write(''.join((x['symbol'] , ',' , x['price'] , ",", "%s"%Da$
-                                #       print("diffrent")
-                                #       print("price has changed " , array,"\r\n")
-                                #       print("Last price was", lastprice,"\r\n")
-                                #       lastprice = array
-                        #print("price is of ",s , "\r\n")
+		#Catch Ctrl+c
 		except KeyboardInterrupt:
 			sys.exit()
+		except:
 			print("Something Gone Wrong Trying Again")
 			GetPrice()
 GetPrice()
